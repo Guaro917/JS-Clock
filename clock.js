548686eeecf5ce9f114af2d28409b3a49f6c5179
Fernando Guarini <fg.xnz1@gmail.com>
2021-01-17 16:40:37 -0300

    Borrando comentarios

--- clock.js
+++ clock.js
@@ -8,7 +8,7 @@ setDate = () => {
     const minutes = now.getMinutes();
     const hours = now.getHours();
 
-    const secondsDegrees = (seconds/60) *360 + 90; //regla de 3 simple
+    const secondsDegrees = (seconds/60) *360 + 90; 
     const minutesDegrees = (minutes/60) * 360 + 90;
     const hoursDegrees = (hours/12) * 360 + 90;
     
